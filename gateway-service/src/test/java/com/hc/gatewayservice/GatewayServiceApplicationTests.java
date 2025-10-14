a6593b4b29fffbbb3703fef99e1399311e4a711e
otunaboEmmanuel <otunabochidubem@gmail.com>
2025-10-15 00:52:48 +0100

    api-gateway-service added

---
+++ gateway-service/src/test/java/com/hc/gatewayservice/GatewayServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hc.gatewayservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
