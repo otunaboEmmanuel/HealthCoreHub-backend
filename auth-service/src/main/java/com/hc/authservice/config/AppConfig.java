19d87db41a4f09284be8eb4cf64800df4fdf89c6
otunaboEmmanuel <otunabochidubem@gmail.com>
2025-10-24 23:01:39 +0100

    cors test 4

--- auth-service/src/main/java/com/hc/authservice/config/AppConfig.java
+++ auth-service/src/main/java/com/hc/authservice/config/AppConfig.java
@@ -20,17 +20,6 @@ public class AppConfig {
         return new RestTemplate();
     }
 
-    @Bean
-    public WebMvcConfigurer corsConfigurer() {
-        return new WebMvcConfigurer() {
-            @Override
-            public void addCorsMappings(CorsRegistry registry) {
-                registry.addMapping("/**")
-                        .allowedOrigins("http://localhost:3000") // frontend URL
-                        .allowedMethods("GET", "POST", "PUT", "DELETE", "OPTIONS")
-                        .allowedHeaders("*")
-                        .allowCredentials(true);
-            }
-        };
-    }
+
+
 }
