73a826c796c3e025ef7abb0b5d24ba54e0cb7775
otunaboEmmanuel <otunabochidubem@gmail.com>
2025-10-31 22:36:49 +0100

    added email service so emails can be sent after status has been updated

---
+++ appointment-service/src/main/java/com/hc/appointmentservice/service/EmailService.java
@@ -0,0 +1,35 @@
+package com.hc.appointmentservice.service;
+
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+@Slf4j
+public class EmailService {
+    public void sendSimpleMail(String email, String firstName, String appointmentTime, String docName) {
+        try {
+            log.info("sending mail to {}", email);
+            SimpleMailMessage mailMessage = new SimpleMailMessage();
+            mailMessage.setTo(email);
+            mailMessage.setSubject("DOCTOR APPOINTMENT UPDATE EMAIL");
+            mailMessage.setText(buildEmailContext(firstName, appointmentTime, docName));
+        }catch (Exception e){
+            log.error(e.getMessage());
+            throw  new RuntimeException(e.getMessage());
+        }
+    }
+
+    private String buildEmailContext(String firstName, String appointmentTime, String docName) {
+        return String.format("""
+                DEAR %s your appointment has been updated
+                you will have your appointment at %s with Dr %s
+                THANK YOU
+                Best regards,
+                The HealthCare Hub Team
+                """, firstName,appointmentTime,docName);
+    }
+
+}
