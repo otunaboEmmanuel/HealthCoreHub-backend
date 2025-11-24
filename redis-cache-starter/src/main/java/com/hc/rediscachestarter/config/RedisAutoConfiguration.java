234c149b1f22a05937d1e288636278281c0ef7e5
otunaboEmmanuel <otunabochidubem@gmail.com>
2025-11-24 23:45:44 +0100

    setting up redis config class, had to change the version of spring from 4.0 to 3.5.6 to use jackson library

--- redis-cache-starter/src/main/java/com/hc/rediscachestarter/config/RedisAutoConfiguration.java
+++ redis-cache-starter/src/main/java/com/hc/rediscachestarter/config/RedisAutoConfiguration.java
@@ -3,9 +3,11 @@ package com.hc.rediscachestarter.config;
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.boot.autoconfigure.AutoConfiguration;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
+import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
 import org.springframework.data.redis.cache.RedisCacheManager;
 import org.springframework.data.redis.connection.RedisConnectionFactory;
@@ -16,13 +18,14 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 
 import java.time.Duration;
 
-@Configuration
+@AutoConfiguration
+@ConditionalOnClass(RedisConnectionFactory.class)
 @EnableCaching
-public class RedisConfig {
+public class RedisAutoConfiguration {
 
     @Bean
+    @ConditionalOnMissingBean
     public RedisTemplate<String, Object> redisTemplate(RedisConnectionFactory connectionFactory) {
-
         RedisTemplate<String, Object> template = new RedisTemplate<>();
         template.setConnectionFactory(connectionFactory);
 
@@ -42,8 +45,8 @@ public class RedisConfig {
     }
 
     @Bean
+    @ConditionalOnMissingBean
     public RedisCacheManager cacheManager(RedisConnectionFactory connectionFactory) {
-
         GenericJackson2JsonRedisSerializer serializer =
                 new GenericJackson2JsonRedisSerializer(new ObjectMapper());
 
