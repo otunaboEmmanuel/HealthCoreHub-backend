9bc67f521b0441bbb5be3cba973af31bcd668eb3
otunaboEmmanuel <otunabochidubem@gmail.com>
2025-11-25 15:02:25 +0100

    testing out fix

--- redis-cache-starter/src/main/java/com/hc/redis/autoconfigure/CustomRedisAutoConfiguration.java
+++ redis-cache-starter/src/main/java/com/hc/redis/autoconfigure/CustomRedisAutoConfiguration.java
@@ -1,13 +1,12 @@
 package com.hc.redis.autoconfigure;
-
 import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.PropertyAccessor;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.boot.autoconfigure.AutoConfiguration;
-import org.springframework.boot.autoconfigure.AutoConfigureAfter;
-import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
+import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
+import org.springframework.boot.autoconfigure.data.redis.RedisAutoConfiguration;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.redis.cache.RedisCacheConfiguration;
@@ -21,7 +20,7 @@ import org.springframework.data.redis.serializer.StringRedisSerializer;
 import java.time.Duration;
 
 @AutoConfiguration
-@AutoConfigureAfter(RedisAutoConfiguration.class)  // ← Add this
+@AutoConfigureBefore(RedisAutoConfiguration.class)
 @ConditionalOnClass(RedisConnectionFactory.class)
 @EnableCaching
 public class CustomRedisAutoConfiguration {
